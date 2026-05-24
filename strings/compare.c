f558d0c15c944c53e51c71dddd9728d608eeb3e5
Krutika Mohanty <mohantykrutika.work247@gmail.com>
2026-05-24 22:46:09 +0530

    Add string comparison without strcmp, returns 1, 0, or -1

--- strings/compare.c
+++ strings/compare.c
@@ -1,12 +1,12 @@
 //Compare two strings without using strcmp() function
 
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 
 int compareStrings(char str1[], char str2[]){
 
     int i = 0;
-    while(str1[i] != '\0' && str2[i] != '\0'){
+    while (str1[i] != '\0' && str2[i] != '\0'){
 
         if (str1[i] == str2[i]){
             i++;
@@ -15,7 +15,7 @@ int compareStrings(char str1[], char str2[]){
         else if (str1[i] > str2[i]){
             return 1;
         }
-        else{
+        else {
             return -1;
         }
     }
@@ -27,7 +27,7 @@ int compareStrings(char str1[], char str2[]){
     else if (str1[i] == '\0'){
         return -1;
     }
-    else{
+    else {
         return 1;
     }
 }
@@ -41,7 +41,7 @@ void displayResult(int result){
         printf("The first string is greater than the second string.");
     }
 
-    else{
+    else {
         printf("The first string is less than the second string.");
     }
 }
